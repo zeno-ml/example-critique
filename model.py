b33f1f2fadcd3680ea880a0aa023e87292404ad0
Alex Cabrera <alex.cabrera@gmail.com>
2023-02-28 15:23:32 -0500

    more metrics

--- model.py
+++ model.py
@@ -19,9 +19,9 @@ def pred_fns(name):
 
 @distill
 def bert_score(df, ops):
-    eval_dict = df[["source", ops.output_column, "label"]].to_dict("records")
+    eval_dict = df[["source", ops.output_column, "reference"]].to_dict("records")
     for d in eval_dict:
-        d["references"] = [d.pop("label")]
+        d["references"] = [d.pop("reference")]
         d["target"] = d.pop(ops.output_column)
 
     result = client.evaluate(
@@ -31,9 +31,72 @@ def bert_score(df, ops):
     return [round(r["value"], 6) for r in result["examples"]]
 
 
+@distill
+def bleu(df, ops):
+    eval_dict = df[[ops.output_column, "reference"]].to_dict("records")
+    for d in eval_dict:
+        d["references"] = [d.pop("reference")]
+        d["target"] = d.pop(ops.output_column)
+
+    result = client.evaluate(
+        metric="bleu",
+        config={"smooth_method": "add_k", "smooth-value": 1.0},
+        dataset=eval_dict,
+    )
+
+    return [round(r["value"], 6) for r in result["examples"]]
+
+
+@distill
+def chrf(df, ops):
+    eval_dict = df[[ops.output_column, "reference"]].to_dict("records")
+    for d in eval_dict:
+        d["references"] = [d.pop("reference")]
+        d["target"] = d.pop(ops.output_column)
+
+    result = client.evaluate(
+        metric="chrf",
+        config={},
+        dataset=eval_dict,
+    )
+
+    return [round(r["value"], 6) for r in result["examples"]]
+
+
+@distill
+def length_ratio(df, ops):
+    eval_dict = df[[ops.output_column, "reference"]].to_dict("records")
+    for d in eval_dict:
+        d["references"] = [d.pop("reference")]
+        d["target"] = d.pop(ops.output_column)
+
+    result = client.evaluate(
+        metric="length_ratio",
+        config={},
+        dataset=eval_dict,
+    )
+
+    return [round(r["value"], 6) for r in result["examples"]]
+
+
 @metric
 def avg_bert_score(df, ops: ZenoOptions):
     return df[ops.distill_columns["bert_score"]].mean()
+
+
+@metric
+def avg_bleu(df, ops: ZenoOptions):
+    return df[ops.distill_columns["bleu"]].mean()
+
+
+@metric
+def avg_chrf(df, ops: ZenoOptions):
+    return df[ops.distill_columns["chrf"]].mean()
+
+
+@metric
+def avg_length_ratio(df, ops: ZenoOptions):
+    return df[ops.distill_columns["length_ratio"]].mean()
 
 
 @distill
